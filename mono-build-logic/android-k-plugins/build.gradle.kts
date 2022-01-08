922588c1a630aa0d5a2e126830824217957544eb
Blundell <blundell.app.dev@gmail.com>
2022-01-08 19:10:36 +0000

    api at top ordering

--- mono-build-logic/android-k-plugins/build.gradle.kts
+++ mono-build-logic/android-k-plugins/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 dependencies {
+    api(project(":android-plugins"))
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.30")
     implementation("com.android.tools.build:gradle:7.0.4")
-    api(project(":android-plugins"))
 }
